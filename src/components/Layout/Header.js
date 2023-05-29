2d633bcb292a08601efb1891a34571ba5bcce1e5
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-05-29 07:18:40 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header>
+        <h1>Mid Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
